0900aa2c274761a1beeb487e10f219e344a1fb83
mandythcuyen <mandythcuyen@gmail.com>
2024-09-15 11:30:34 +0700

    (Fix) Replace current_action logic and refactor some code lines

--- app/javascript/dashboard/mixins/contactMixin.js
+++ app/javascript/dashboard/mixins/contactMixin.js
@@ -15,5 +15,29 @@ export default {
 
       return actionText;
     },
+    currentConversationPlanText(conversation_plans) {
+      if (!conversation_plans.length) return '';
+
+      const latest_conversation_plan = conversation_plans.find(
+        conversation_plan => conversation_plan.completed_at === null
+      );
+      if (!latest_conversation_plan) return '';
+
+      const {
+        description,
+        status,
+        action_date: actionDate,
+        inbox_type: inboxType,
+      } = latest_conversation_plan;
+
+      const status_text = this.$t(
+        'CHAT_LIST.CHAT_STATUS_FILTER_ITEMS.' + status + '.TEXT'
+      );
+      const actionDateText = format(new Date(actionDate), 'dd/MM/yy');
+      const actionText = `${inboxType} / ${status_text}  (${actionDateText})`;
+      if (description) return `${description} / ${actionText}`;
+
+      return actionText;
+    },
   },
 };
